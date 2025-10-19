31d13b3fbc16ffb519f07511fef0c43cec902c9b
Boumitran Katiresan <boumitran@32studio.org>
2025-10-19 15:37:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "mouth-metrics",
+  "appId": "1:164502969077:web:74881f2062fbc9ef1403bc",
+  "apiKey": "AIzaSyB8zjuk5xuyGBAqdHa71hg_kpmBuNVJcow",
+  "authDomain": "mouth-metrics.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "164502969077"
+};
