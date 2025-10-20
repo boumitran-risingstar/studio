f7dd2e0aab41e7f6b59f6a3728e351229b3a9fa5
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 01:57:10 +0000

    change /u/{slug} to /user/{slug}

--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useEffect, useState } from "react";
@@ -98,4 +97,3 @@ export default function UserSlugPage({ params }: { params: { slug: string } }) {
         </div>
     );
 }
-
