a8d81cc0abd98376cf3dc4abddcd322c44c5f8c4
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 03:59:08 +0000

    change http://localhost:8080/getSlugData to https://users-164502969077.a

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -56,7 +56,7 @@ export async function getUserFromExternalApi(uid: string) {
 
 export async function getSlugDataFromExternalApi(slug: string) {
     try {
-        const response = await fetch(`http://localhost:8080/getSlugData?slugURL=${slug}`);
+        const response = await fetch(`https://users-164502969077.asia-southeast1.run.app/getSlugData?slugURL=${slug}`);
         if (!response.ok) {
             const apiError = await response.json();
             return { success: false, error: apiError.message || 'Could not find user.' };
@@ -67,4 +67,5 @@ export async function getSlugDataFromExternalApi(slug: string) {
         return { success: false, error: 'A network error occurred.' };
     }
 }
+
 
