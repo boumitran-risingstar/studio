603ee793a2f62e94c4950777db81e4aee477f1b0
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 09:45:52 +0000

    after login redirect to /

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState } from 'react';
@@ -53,7 +54,7 @@ export function LoginForm() {
         return;
       }
       
-      router.push('/dashboard');
+      router.push('/');
     } catch (error: any) {
       let errorMessage = "An unexpected error occurred.";
       if (error.code) {
