db33ba6dcf0146cda2db47ec3a0ab5515b1e08e0
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 12:11:44 +0000

    ceb9e9aa-72d7303d2f172ccd.js:1 Cross-Origin-Opener-Policy policy would b

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
