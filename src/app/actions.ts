b56c5b56778fe30ea19a89f3be788b7fc43a8f0a
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 03:55:07 +0000

    on opening /user/<slugURL> make an external call from backend to http://

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -56,7 +56,7 @@ export async function getUserFromExternalApi(uid: string) {
 
 export async function getSlugDataFromExternalApi(slug: string) {
     try {
-        const response = await fetch(`https://users-164502969077.asia-southeast1.run.app/getSlugData?slug=${slug}`);
+        const response = await fetch(`http://localhost:8080/getSlugData?slugURL=${slug}`);
         if (!response.ok) {
             const apiError = await response.json();
             return { success: false, error: apiError.message || 'Could not find user.' };
@@ -67,3 +67,4 @@ export async function getSlugDataFromExternalApi(slug: string) {
         return { success: false, error: 'A network error occurred.' };
     }
 }
+
