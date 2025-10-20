8d391f7279a9c2cca6e7cc5b6b0b29b19896017e
Boumitran Katiresan <boumitran@32studio.org>
2025-10-20 06:14:55 +0000

    remove all content in /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,4 @@
-"use client";
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useUser } from '@/firebase';
-import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Home() {
-  const { user, isUserLoading } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isUserLoading) {
-      if (user) {
-        router.replace('/dashboard');
-      } else {
-        router.replace('/login');
-      }
-    }
-  }, [user, isUserLoading, router]);
-
-  return (
-      <div className="flex items-center justify-center h-screen bg-background">
-        <div className="w-full h-full flex items-center justify-center">
-             <Skeleton className="h-24 w-24 rounded-full" />
-        </div>
-      </div>
-  );
+  return null;
 }
